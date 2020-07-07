4cffef42732944d4b81b3e62a03d4040ad74f185
AlteredCoder <AlteredCoder>
2020-07-07 12:28:42 +0200

    update

--- pkg/cwhub/hubMgmt.go
+++ pkg/cwhub/hubMgmt.go
@@ -31,9 +31,9 @@ var ItemTypes = []string{PARSERS, PARSERS_OVFLW, SCENARIOS, COLLECTIONS}
 
 var HubIdx map[string]map[string]Item
 
-var Installdir = "/etc/crowdsec/"
-var Hubdir = "/etc/crowdsec/cscli/hub/"
-var Cfgdir = "/etc/crowdsec/cscli/"
+var Installdir = "/etc/crowdsec/config/"
+var Hubdir = "/etc/crowdsec/config/cscli/hub/"
+var Cfgdir = "/etc/crowdsec/config/cscli/"
 
 var RawFileURLTemplate = "https://raw.githubusercontent.com/crowdsecurity/hub/%s/%s"
 var HubIndexFile = ".index.json"
